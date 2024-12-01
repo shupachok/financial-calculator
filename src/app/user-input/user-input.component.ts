4f0e909cb6b7836625dd8e13128405e9f0569ff3
SUPACHOK\shupa <shupachok@gmail.com>
2024-12-01 16:03:28 +0700

    update: change template driven form to reactive form

--- src/app/user-input/user-input.component.ts
+++ src/app/user-input/user-input.component.ts
@@ -1,12 +1,13 @@
 import {Component, EventEmitter, inject, Output} from '@angular/core';
-import {FormsModule} from '@angular/forms';
+import {FormControl, FormGroup, FormsModule, ReactiveFormsModule, Validators} from '@angular/forms';
 import {UserInputService} from './user-input.service';
 import {InvestmentResult} from '../investment-results/investment-result.model';
 
 @Component({
   selector: 'app-user-input',
   imports: [
-    FormsModule
+    FormsModule,
+    ReactiveFormsModule
   ],
 
   templateUrl: './user-input.component.html',
@@ -14,25 +15,32 @@ import {InvestmentResult} from '../investment-results/investment-result.model';
   styleUrl: './user-input.component.css'
 })
 export class UserInputComponent {
-
   @Output() calculate = new EventEmitter<InvestmentResult[]>();
+  userInputForm = new FormGroup({
+    initialInvestment: new FormControl('', [Validators.required,Validators.min(0)]),
+    annualInvestment: new FormControl('', [Validators.required,Validators.min(0)]),
+    expectedReturn: new FormControl('', [Validators.required,Validators.min(0)]),
+    duration: new FormControl('', [Validators.required,Validators.min(0)])
+  })
 
   // submit = output<InvestmentResult[]>()
-  enteredInitialInvestment:number=0.00;
-  enteredAnnualInvestment:number=0.00;
-  enteredExpectedReturn:number=0.00;
-  enteredDuration:number=0;
+  // enteredInitialInvestment:number=0.00;
+  // enteredAnnualInvestment:number=0.00;
+  // enteredExpectedReturn:number=0.00;
+  // enteredDuration:number=0;
 
   private userInputService = inject(UserInputService);
 
   onSubmit() {
     console.log('SUBMIT!');
+
+    let formInputValue = this.userInputForm.value;
     this.calculate.emit(
       this.userInputService.getInvestmentResult({
-          enteredInitialInvestment:this.enteredInitialInvestment,
-          enteredExpectedReturn:this.enteredExpectedReturn,
-          enteredAnnualInvestment:this.enteredAnnualInvestment,
-          enteredDuration:this.enteredDuration
+          enteredInitialInvestment: Number(formInputValue.initialInvestment),
+          enteredExpectedReturn: Number(formInputValue.expectedReturn),
+          enteredAnnualInvestment:Number(formInputValue.annualInvestment),
+          enteredDuration: Number(formInputValue.duration)
         }));
   }
 }
